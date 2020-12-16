caf402fa50bdde279f411b96a77093956b308588
jxnprince <68714241+jxnprince@users.noreply.github.com>
2020-12-16 16:08:06 -0700

    Almost done with one

--- 01-big-octopus.js
+++ 01-big-octopus.js
@@ -12,26 +12,20 @@ function quadraticBiggestFish(fishes) {
   return fishes[fishes.length - 1];
 }
 
-// const fishies = ['fish', 'fiiiissshhhhhh', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish',
-// 'ffiiiiisshh', 'fsh'];
-// console.log(quadraticBiggestFish(fishies));
-// => "fiiiissshhhhhh";
-
-function nlognBiggestFish(fishes) {
-  // Code goes here ...
-}
-
-
-function linearBiggestFish(fishes) {
-  // Code goes here ...
-}
-
-
-tilesArray = ["up", "right-up", "right", "right-down", "down", "left-down", "left", "left-up"]
-function slowDance(direction, tilesArray) {
-  // Code goes here ...
-}
-
+ nlognBiggestFish = (fishes) => fishes.sort((a, b) => a.length - b.length)[fishes.length - 1]
+ 
+ function linearBiggestFish(fishes) {
+   let largestFish = fishes[0]
+   for (let i = 1; i < fishes.length; i++) {
+     if (fishes[i].length > largestFish.length) largestFish = fishes[i];
+   }
+   return largestFish;
+ }
+  
+ tilesArray = ["up", "right-up", "right", "right-down", "down", "left-down", "left", "left-up"]
+ function slowDance(direction, tilesArray) {
+   
+  }
 
 tilesObj = {
   "up": 0,
@@ -43,6 +37,6 @@ tilesObj = {
   "left": 6,
   "left-up": 7
 }
-function fastDance(direction, tilesObj) {
-  // Code goes here ....
-}
+// function fastDance(direction, tilesObj) {
+//   // Code goes here ....
+// }
