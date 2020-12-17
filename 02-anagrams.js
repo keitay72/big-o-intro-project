63d8b1d706722ba54481a1deed71968b3c27e08d
jxnprince <68714241+jxnprince@users.noreply.github.com>
2020-12-16 17:17:43 -0700

    Anagrams in progress

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -1,19 +1,25 @@
 function firstAnagram(str1, str2) {
   for (let i = 0; i < str1.length; i++) {
     const el = str1[i];
-    if (el !== str2.slice(str2.length - 1)) {
-      return false;
+    let targetIdx = str2.indexOf(el)
+    if (targetIdx === -1) return false;
+    else if (el === str2[targetIdx]) {
+      str2 = str2.slice(0, targetIdx) + str2.slice(targetIdx + 1)
     }
   }
-  return true;
+  return str2.length === 0
 }
 
-console.log(firstAnagram("gizmo", "sally"));    // => false
-console.log(firstAnagram("elvis", "lives"));    // => true
+// console.log(firstAnagram("gizmo", "sally"));    // => false
+// console.log(firstAnagram("elvis", "lives"));    // => true
 
-function secondAnagram(str1, str2) {
-  // Code goes here ....
+secondAnagram = (str1, str2) => {
+  return str1.split('').sort((a, b) => a - b).join('') == str2.split('').sort((a, b) => a - b).join('')
 }
+
+
+console.log(secondAnagram("gizmo", "sally"));    // => false
+console.log(secondAnagram("elvis", "lives"));    // => true
 
 
 function thirdAnagram(str1, str2) {
